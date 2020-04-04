ec88b3979ca347d8db7fff4374026d4860e0e358
Merhat Veliev <merhat.veliev@abv.bg>
2020-04-04 19:46:05 +0300

    Remove unnecessary lines

--- main.cpp
+++ main.cpp
@@ -1,25 +1,9 @@
 #include <iostream>
-#include <string>
 #include "chess.h"
-
-using namespace std;
 
 int main()
 {
 	Board board;
-	string str;
-	bool newgame = true;
-	cout << "Enter any key to continue" << endl;
-	cin >> str;
-
-	while(newgame){
-		board.setBoard();
-		while (board.playGame());
-		cout << "Do you want to play again? (y for yes, anything else for no) ";
-		cin >> str;
-		if (str != "y")
-			newgame = false;
-	}
-	
+	while (board.playGame());
 	return 0;
 }
