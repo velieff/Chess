361148a48039dbb4480b35a6bc0ff4969628ba30
Merhat Veliev <merhat.veliev@abv.bg>
2020-04-05 18:44:48 +0300

    Add version with polymorphism

---
+++ Knight.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include<iostream>
+#include<fstream>
+#include"Figure.h"
+
+class Knight : public Figure
+{
+public:
+	Knight() {};
+	virtual void print(bool isWhite) { isWhite ? std::cout << " H " : std::cout << " h "; };
+	virtual bool isValidMove(int currentX, int currentY, int newX, int newY)
+	{
+		if ((abs(currentX - newX) == 2 && abs(currentY - newY) == 1) || (abs(currentX - newX) == 1 && abs(currentY - newY) == 2))
+		{
+			return true;
+		}
+		else
+			return false;
+	};
+	virtual bool isCleanWay(Board& board, Square* currentSquare, Square* newSquare) { return true; };
+	virtual Figure* getCopy() const { return new Knight(*this); };
+	virtual bool isKing() { return false; };
+}; 
---
+++ Pawn.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include<iostream>
+#include<fstream>
+#include"Figure.h"
+
+class Pawn : public Figure
+{
+public:
+	Pawn() {};
+	virtual void print(bool isWhite) {isWhite ? std::cout << " P " : std::cout << " p "; };
+	virtual bool isValidMove(int currentX, int currentY, int newX, int newY) 
+	{
+		if ((currentX + 1 == newX || currentX - 1 == newX || currentX == newX)
+			&& newY == currentY + 1)
+		{
+			return true;
+		}
+		return false;
+	};
+	virtual bool isCleanWay(Board& board, Square* currentSquare, Square* newSquare) { return true; };
+	virtual Figure* getCopy() const { return new Pawn(*this); };
+	virtual bool isKing() { return false; };
+};
