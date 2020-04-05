9f322f257d613df21fb67261a65196d0885b56f0
Merhat Veliev <merhat.veliev@abv.bg>
2020-04-05 16:33:03 +0300

    Update chess.h

--- chess.h
+++ chess.h
@@ -4,8 +4,10 @@
 #include <cmath>
 #include <string>
 
-enum Piece { KING, QUEEN, BISHOP, KNIGHT, ROOK, PAWN, EMPTY };
+enum Piece { ROOK, KNIGHT, BISHOP, QUEEN, KING, PAWN, EMPTY};
 enum Color { WHITE, BLACK, NONE };
+
+const int boardSize = 8;
 
 class Square
 {
@@ -15,29 +17,30 @@ private:
 	int x, y;
 public:
 	Square();
-	void setSpace(Square*);
+	void copy(Square* s);
+	void print();
+	void setPieceAndColor(Piece p, Color c);
 	void setEmpty();
-	void setPieceAndColor(Piece, Color);
 	Piece getPiece();
 	Color getColor();
-	void setX(int newX) { x = newX; }
-	void setY(int newY) { y = newY; }
-	int getX() { return x; }
-	int getY() { return y; }
+	void setX(int newX);
+	void setY(int newY);
+	int getX();
+	int getY();
 };
 
 class Board
 {
 private:
-	Square square[8][8];
-	Color turn=WHITE;
+	Square square[boardSize][boardSize];
+	Color turn = WHITE;
+	bool moveFigure(int currentX, int currentY, int newX, int newY);
 	bool moveKing(Square* currentSquare, Square* newSquare);
 	bool moveQueen(Square* currentSquare, Square* newSquare);
 	bool moveBishop(Square* currentSquare, Square* newSquare);
 	bool moveKnight(Square* currentSquare, Square* newSquare);
 	bool moveRook(Square* currentSquare, Square* newSquare);
 	bool movePawn(Square* currentSquare, Square* newSquare);
-	bool moveFigure(int currentX, int currentY, int newX, int newY);
 	void printBoard();
 public:
 	Board();
@@ -46,4 +49,3 @@ public:
 	bool makeMove();
 	bool playGame();
 };
-
